49cdc10567e05d590bf6f1ff01f632c0ceb3c5ac
lleotrnn <114983264+lleotrnn@users.noreply.github.com>
2023-05-26 17:11:58 +0200

    gui

---
+++ src/main/java/com/isep/sixquiprend/GUI/GameController.java
@@ -0,0 +1,42 @@
+package com.isep.sixquiprend.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class GameController {
+
+    @FXML
+    private Text beefHeadTextPlayer1;
+
+    @FXML
+    private Text beefHeadTextPlayer2;
+
+    @FXML
+    private Text beefHeadTextPlayer3;
+
+    @FXML
+    private Text beefHeadTextPlayer4;
+
+    @FXML
+    private VBox boxPlayer1;
+
+    @FXML
+    private VBox boxPlayer2;
+
+    @FXML
+    private VBox boxPlayer21;
+
+    @FXML
+    private VBox boxPlayer3;
+
+    @FXML
+    private VBox boxPlayer4;
+
+    @FXML
+    private Text nameLabel;
+
+    @FXML
+    private Text roundLabel;
+
+}
